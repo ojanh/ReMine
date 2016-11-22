0844aaef5713c88de8c66195374210c986fe068e
ojanh <ojan.fauzans@gmail.com>
2016-11-23 01:46:32 +0700

    Massive Changes 3

--- app/src/main/java/com/example/ojan_/remine/DBcomms.java
+++ app/src/main/java/com/example/ojan_/remine/DBcomms.java
@@ -11,8 +11,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.net.URLEncoder;
 
@@ -22,99 +24,23 @@ import java.net.URLEncoder;
  */
 
 public class DBcomms {
-    String Query; //tipe query hanya CheckData,GetData,SetData
-    String LoginIP = "http://127.0.0.1/"; //URL link ke php (belom ada)
-    String roleLogin;
-    //constructors
-    public DBcomms(String query) {
-         Query = query;
-    }
+
+    String LoginIP = "http://10.5.79.162/"; //IP Address
+
 
     //set IP  baru kalau IP nya bukan Localhost (127.0.0.1)
-    public void setCustomIP(String newIP){
-        LoginIP=newIP;
+    public void setCustomIP(String newIP) {
+        LoginIP = newIP;
     }
 
-    //untuk cek data (Buat Login)
-    public String checkData(String type){
+        //untuk cek data (Buat Login)
+    public String checkData(String EncodetoPHP) {
 
-        String result="";
-        //setup koneksi dengan PHP
-                try {
-
-                    //setup dengan HTTP port dengan metode POST request
-                    URL linkURL = new URL(LoginIP + "check_data.php"); //setURL
-                    HttpURLConnection linkcon = (HttpURLConnection) linkURL.openConnection(); //open port 80 connection
-                    linkcon.setRequestMethod("POST"); //request http method
-                    linkcon.setDoOutput(true); //set data ke PHP
-                    linkcon.setDoInput(true); //get data dari PHP
-
-                    //kirim data ke PHP buat SQLquerying
-                    OutputStream outStream=linkcon.getOutputStream(); //set untuk kirim data
-                    BufferedWriter buffWrite = new BufferedWriter(new OutputStreamWriter(outStream,"UTF-8")); //setup text pengiriman
-
-                    //setup POST HTTP Method yang akan dikirm
-                    String Checkdata_inPHP = URLEncoder.encode("sQuery","UTF-8")+"="+URLEncoder.encode(Query,"UTF-8")
-                            +"&"+URLEncoder.encode("type","UTF-8")+"="+URLEncoder.encode("type","UTF-8");
-
-                    //kirim data ke PHP
-                    buffWrite.write(Checkdata_inPHP);
-                    buffWrite.flush(); //hapus buffer
-                    buffWrite.close(); //close buffer
-
-                    //Ambil data dari PHP (Hasil SQLquerying)
-                    InputStream inStream=linkcon.getInputStream();
-                    BufferedReader buffRead = new BufferedReader(new InputStreamReader(inStream, "iso-8859-1"));
-
-                    //inisial result data
-                    
-                    String line=""; //variabel diambil dari bufferreader data dari PHP
-                    int lineCount = 0;
-
-                    //write data dari php echo ke var line lalu add ke result
-                    while((line = buffRead.readLine()) != "$"){
-                        result += line;
-                        lineCount++;
-                    }
-
-                    buffRead.skip(lineCount);
-
-                    while ((line = buffRead.readLine()) != null){
-                        roleLogin += line;
-
-                    }
-                    //close read
-                    buffRead.close();
-                    inStream.close();
-
-                    //close http Connection
-                    linkcon.disconnect();
-
-                    //return hasil result
-                    
-
-
-                } catch (IOException e){
-                    e.printStackTrace();
-                }
-
-            return result;
-
-            }
-
-    //untuk ambil roleLogin
-    public String getRoleLogin(){
-        return roleLogin;
-    }
-
-    //getQuery (Buat Request SQL table)
-    //result berupa JSON Object
-    public String getQuery(){
-
-        String result="";
-
+        String result = "";
         //setup koneksi dengan PHP
         try {
+
+            //setup dengan HTTP port dengan metode POST request
             URL linkURL = new URL(LoginIP + "check_data.php"); //setURL
             HttpURLConnection linkcon = (HttpURLConnection) linkURL.openConnection(); //open port 80 connection
             linkcon.setRequestMethod("POST"); //request http method
@@ -122,31 +48,40 @@ public class DBcomms {
             linkcon.setDoInput(true); //get data dari PHP
 
             //kirim data ke PHP buat SQLquerying
-            OutputStream outStream=linkcon.getOutputStream(); //set untuk kirim data
-            BufferedWriter buffWrite = new BufferedWriter(new OutputStreamWriter(outStream,"UTF-8")); //setup text pengiriman
+            OutputStream outStream = linkcon.getOutputStream(); //set untuk kirim data
+            BufferedWriter buffWrite = new BufferedWriter(new OutputStreamWriter(outStream, "UTF-8")); //setup text pengiriman
 
             //setup POST HTTP Method yang akan dikirm
-            String Checkdata_inPHP = URLEncoder.encode("sQuery","UTF-8")+"="+URLEncoder.encode(Query,"UTF-8")
-                    +"&"+URLEncoder.encode("type","UTF-8")+"="+URLEncoder.encode(roleLogin,"UTF-8");
+
 
             //kirim data ke PHP
-            buffWrite.write(Checkdata_inPHP);
+
+            buffWrite.write(EncodetoPHP);
             buffWrite.flush(); //hapus buffer
             buffWrite.close(); //close buffer
 
             //Ambil data dari PHP (Hasil SQLquerying)
-            InputStream inStream=linkcon.getInputStream();
-            BufferedReader buffRead = new BufferedReader(new InputStreamReader(inStream, "iso-8859-1"));
+            InputStream inStream = linkcon.getInputStream();
+            BufferedReader buffRead = new BufferedReader(new InputStreamReader(inStream, "UTF-8"));
 
             //inisial result data
-
-            String line=""; //variabel diambil dari bufferreader data dari PHP
-
+            String line; //variabel diambil dari bufferreader data dari PHP
+            int lineCount = 0;
+            StringBuilder sb = new StringBuilder();
 
             //write data dari php echo ke var line lalu add ke result
-            while((line = buffRead.readLine()) != null){
-                result += line;
+            while ((line = buffRead.readLine()) != null) {
+                sb.append(line + "\n");
+
             }
+            result = sb.toString();
+                    /*
+                    buffRead.skip(lineCount);
+
+                    while ((line = buffRead.readLine()) != null){
+                        roleLogin += line;
+
+                    }*/
 
             //close read
             buffRead.close();
@@ -155,27 +90,84 @@ public class DBcomms {
             //close http Connection
             linkcon.disconnect();
 
-            //return hasil result
 
-
-
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
+        //return hasil result
 
         return result;
     }
 
-    //setQuery (Data Manipulation (Update dan insert into))
-    public void setQuery(){
+       //getQuery (Buat Request SQL table)
+    //result berupa JSON Object
+    public String getQuery(String postEncode) {
 
+        String result = "";
+        result=HTTPconnection("get_data.php", postEncode);
+
+        return result;
+    }
+
+
+    //setQuery (Data Manipulation (Update dan insert into))
+    public void setQuery(String postEncode) {
+        String result="";
+       result=HTTPconnection("set_data.php", postEncode);
 
 
     }
 
+    //koneksi dengan HTTP (Function)
+    String HTTPconnection(String PHP_FilePath, String PostData) {
+        String result = "";
+        try {
+
+            //setup dengan HTTP port dengan metode POST request
+            URL linkURL = new URL(LoginIP + PHP_FilePath); //setURL
+            HttpURLConnection linkcon = (HttpURLConnection) linkURL.openConnection(); //open port 80 connection
+            linkcon.setRequestMethod("POST"); //request http method
+            linkcon.setDoOutput(true); //set data ke PHP
+            linkcon.setDoInput(true); //get data dari PHP
 
 
+            //kirim data ke PHP buat SQLquerying
+            OutputStream outStream = linkcon.getOutputStream(); //set untuk kirim data
+            BufferedWriter buffWrite = new BufferedWriter(new OutputStreamWriter(outStream, "UTF-8")); //setup text pengiriman
+
+            //setup POST HTTP Method yang akan dikirm
+            //kirim data ke file PHP
+            buffWrite.write(PostData);
+            buffWrite.flush(); //hapus buffer
+            buffWrite.close(); //close buffer
+
+            //Ambil data dari PHP (Hasil SQLquerying)
+            InputStream inStream = linkcon.getInputStream();
+            BufferedReader buffRead = new BufferedReader(new InputStreamReader(inStream, "UTF-8"));
+
+            //inisial result data
+            String line; //variabel diambil dari bufferreader data dari PHP
+            StringBuilder sb = new StringBuilder();
+
+            //write data dari php echo ke var line lalu build di variabel sb
+            while ((line = buffRead.readLine()) != null) {
+                sb.append(line + "\n");
+
+            }
+            result = sb.toString();
+
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 }
-
 
 
--- app/src/main/java/com/example/ojan_/remine/LoginMenu.java
+++ app/src/main/java/com/example/ojan_/remine/LoginMenu.java
@@ -2,7 +2,9 @@ package com.example.ojan_.remine;
 
 import android.app.ActionBar;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,6 +14,13 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 public class LoginMenu extends AppCompatActivity {
 
@@ -34,40 +43,42 @@ public class LoginMenu extends AppCompatActivity {
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog dialoggetQuery;
-                dialoggetQuery= new AlertDialog.Builder(LoginMenu.this).create();
-                dialoggetQuery.setTitle("Login Status");
-                dialoggetQuery.setMessage("Lagi check");
-                dialoggetQuery.show();
 
-                //pindah ke background tasking
+                //pindah ke Class Login (Inner Class)
+                //melakukan komunikasi ke server untuk cek apa user dan password sesuai dengan di server
                 Login login = new Login();
                 login.execute(usernam.getText().toString(), passw.getText().toString());
+
+                String role=login.getRoles();
+
+                if (role=="toko"){
+                    Intent pindah = new Intent(getApplicationContext(),toko_menu.class);
+                    pindah.putExtra("username",usernam.getText().toString());
+                    startActivity(pindah);
+                }
+
 
 
 
                 //pindah activity ke MainMenu (Temp ke UserView)
+                /*
                 Intent pindah = new Intent(getApplicationContext(),MainMenu.class);
                 pindah.putExtra("username",usernam.getText().toString());
-                startActivity(pindah);
+                startActivity(pindah);*/
             }
 
         });
     }
 
 
-
-
-
-
-
-
-    //fungsi
+    //fungsi bentuk class Login
     class Login extends AsyncTask<String,Void,Void> {
 
-
         AlertDialog dialoggetQuery; //getquery dialog
-        String hasil;
+        String hasil_temp;
+        String roles;
+        String message;
+
 
         @Override
         protected Void doInBackground(String... params) {
@@ -75,10 +86,27 @@ public class LoginMenu extends AppCompatActivity {
             String username=params[0];
             String password=params[1];
 
-            DBcomms checkLogin = new DBcomms
-                    ("SELECT * FROM users WHERE user_id '" + username + "' AND passsword='" + password + "'");
+            //eksekusi data dari username dan password
+            DBcomms checkLogin = new DBcomms();
+            String postEncode = "";
+            try {
+                postEncode = URLEncoder.encode("username","UTF-8")+"="+URLEncoder.encode(username,"UTF-8")
+                        +"&"+URLEncoder.encode("password","UTF-8")+"="+URLEncoder.encode(password, "UTF-8")
+                        +"&"+URLEncoder.encode("type","UTF-8")+"="+URLEncoder.encode("login","UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
 
-            hasil= checkLogin.checkData("login");
+            hasil_temp=checkLogin.checkData(postEncode);
+
+            //konversi JSON ke string untuk data
+            try {
+                JSONObject assemb_result = new JSONObject(hasil_temp);
+                roles=assemb_result.getString("roles");
+                message=assemb_result.getString("info");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
 
 
             return null;
@@ -91,20 +119,33 @@ public class LoginMenu extends AppCompatActivity {
             dialoggetQuery.setMessage("Lagi check");
             dialoggetQuery.show();
 
+
         }
-
-
-
-
 
         @Override
         protected void onPostExecute(Void aVoid) {
-            dialoggetQuery.setMessage("Login " + hasil);
-            dialoggetQuery.show();
 
-            }
+            dialoggetQuery.setMessage(message);
+            dialoggetQuery.show();
+            dialoggetQuery.setCanceledOnTouchOutside(true);
+
+        }
+
+        public String getRoles(){
+            return roles;
+        }
+
+    }
+
+    //fungsi save data dengan shared preferences
+    private void shpref (String username, String IP_addr){
+        SharedPreferences shpref = getSharedPreferences("Init", Context.MODE_PRIVATE);
+        SharedPreferences.Editor sheditor = shpref.edit();
+        sheditor.putString("username", username);
+        sheditor.putString("IP_address", IP_addr);
+        sheditor.commit();
+
     }
 }
-
 
 
